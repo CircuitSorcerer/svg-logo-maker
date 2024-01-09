061d94891733f96865e4ee1238abcf048e0e95b1
Van Gogh <139753682+CircuitSorcerer@users.noreply.github.com>
2024-01-10 00:44:25 +1100

    Started constructing classes and index, too tired, going to bed.

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require ('inquirer');
-const { init } = require ('./lib/shapes')
+//const { init } = require ('./lib/shapes')
+
+const question = [
+    {
+        type: 'input',
+        message: 'Enter the three characters to be placed on the logo.',
+        name: 'text',
+    },
+
+    {
+        type: 'input',
+        message: 'Choose a colour for the text using the keyword or the hexadecimal value.',
+        name: 'textColour',
+    },
+
+    {
+        type: 'list',
+        message: 'Which shape?',
+        name: 'shapeChoice',
+        choices: ['Rectangle', 'Circle', 'Triangle'],
+    },
+
+    {
+        type: 'input',
+        message: 'Choose a colour for the shape using the keyword or the hexadecimal value',
+        name: 'shapeColour',
+    },
+];
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            
+        })
+}
